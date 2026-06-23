77c3566f95616e0b2f7c5188ff74e0bd735781c3
jaehunshin <jhshin0422@gmail.com>
2026-06-24 03:36:11 +0900

    :tada: start: Spring Boot 프로젝트 초기 생성 파일 추가

---
+++ src/main/java/org/alty/altyserver/AltyServerApplication.java
@@ -0,0 +1,13 @@
+package org.alty.altyserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AltyServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AltyServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/alty/altyserver/AltyServerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.alty.altyserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AltyServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
