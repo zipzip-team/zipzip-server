1460d40078eb05870bc574d413cc154de7730366
jaehunshin <jhshin0422@gmail.com>
2026-06-26 15:07:27 +0900

    :wrench: settings: JPA Audit, BaseTimeEntity 추가

---
+++ src/main/java/org/alty/altyserver/global/jpa/JpaAuditingConfig.java
@@ -0,0 +1,8 @@
+package org.alty.altyserver.global.jpa;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing
+public class JpaAuditingConfig {}
