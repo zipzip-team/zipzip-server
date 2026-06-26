9350642a530a2a337f1f02ab0dcf23e3671eecf9
jaehunshin <jhshin0422@gmail.com>
2026-06-26 15:07:53 +0900

    :wrench: settings: Swagger 문서 설정 추가

---
+++ src/main/java/org/alty/altyserver/global/config/SwaggerConfig.java
@@ -0,0 +1,20 @@
+package org.alty.altyserver.global.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .info(
+                        new Info()
+                                .title("Alty Server API")
+                                .description("Alty Server API 문서")
+                                .version("v1.0.0"));
+    }
+}
