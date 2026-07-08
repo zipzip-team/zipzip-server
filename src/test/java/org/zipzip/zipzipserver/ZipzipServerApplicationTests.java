b9239f0fdcb01fa4f3a7dfabb5b307c771ce0293
jaehunshin <jhshin0422@gmail.com>
2026-07-08 20:14:43 +0900

    :wrench: settings: JWT 테스트 설정 추가

--- src/test/java/org/zipzip/zipzipserver/ZipzipServerApplicationTests.java
+++ src/test/java/org/zipzip/zipzipserver/ZipzipServerApplicationTests.java
@@ -3,7 +3,13 @@ package org.zipzip.zipzipserver;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(
+        properties = {
+            "jwt.issuer=zipzip-server-test",
+            "jwt.secret=test-jwt-secret-must-be-at-least-32-characters",
+            "jwt.access-token-expiration=30m",
+            "jwt.refresh-token-expiration=14d"
+        })
 class ZipzipServerApplicationTests {
 
     @Test
