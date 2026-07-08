4898f5078a8b19c19f90abd80efa32109a25c4b4
jaehunshin <jhshin0422@gmail.com>
2026-07-08 23:57:50 +0900

    :bug: fix: CI 테스트 Apple 설정 누락 수정

--- src/test/java/org/zipzip/zipzipserver/ZipzipServerApplicationTests.java
+++ src/test/java/org/zipzip/zipzipserver/ZipzipServerApplicationTests.java
@@ -8,7 +8,11 @@ import org.springframework.boot.test.context.SpringBootTest;
             "jwt.issuer=zipzip-server-test",
             "jwt.secret=test-jwt-secret-must-be-at-least-32-characters",
             "jwt.access-token-expiration=30m",
-            "jwt.refresh-token-expiration=14d"
+            "jwt.refresh-token-expiration=14d",
+            "apple.team-id=test-apple-team-id",
+            "apple.client-id=test-apple-client-id",
+            "apple.key-id=test-apple-key-id",
+            "apple.private-key=test-apple-private-key"
         })
 class ZipzipServerApplicationTests {
 
