c709fa362fe0acc83d51a10e850fb1728db67ebe
jaehunshin <jhshin0422@gmail.com>
2026-07-10 20:35:18 +0900

    :recycle: refactor: 통합 테스트 mock 어노테이션 교체

--- src/test/java/org/zipzip/zipzipserver/domain/reaction/controller/ReactionControllerIntegrationTest.java
+++ src/test/java/org/zipzip/zipzipserver/domain/reaction/controller/ReactionControllerIntegrationTest.java
@@ -21,10 +21,10 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.transaction.annotation.Transactional;
 import org.testcontainers.containers.PostgreSQLContainer;
@@ -67,7 +67,7 @@ class ReactionControllerIntegrationTest {
     @Autowired private EntityManager entityManager;
     @Autowired private JwtTokenProvider jwtTokenProvider;
 
-    @MockBean private ObjectStorageService objectStorageService;
+    @MockitoBean private ObjectStorageService objectStorageService;
 
     private AppUser uploader;
     private AppUser member;
